73fc81b92397c5b5ebcf65ca54cbd579539bbf7e
bjmayor <415074476@qq.com>
2016-03-28 08:47:19 +0800

    u

--- advanced_programming_in_the_unix_environment/apue.h
+++ advanced_programming_in_the_unix_environment/apue.h
@@ -20,6 +20,50 @@
 #define MAXLINE 4096
 
 #define FILE_MODE (S_IRUSER | S_IWUSER | S_IRGRP | S_IROTH)
+#define DIR_MODE (FILE_MODE | S_IXUSER | S_IXGRP | S_IXOTH)
+
+typedef void Sigfunc(int);
+
+#if defined(SIG_IGN) && !defined(SIG_ERR)
+#define SIG_ERR ((Sigfunc *)-1)
+#endif
+
+#define min(a,b) ((a)<(b) ? (a) : (b))
+#define max(a,b) ((a)>(b) ? (a) : (b))
+
+char *path_alloc(int*);
+long open_max(void);
+void clr_fl(int, int);
+void set_fl(int, int);
+void pr_mask(const char *);
+Sigfunc *signal_intr(int, Sigfunc*);
+
+int tty_cbreak(int);
+int tty_raw(int);
+int tty_reset(int);
+void tty_atextit(void);
+
+#ifdef ECHO 
+struct termino *tty_termios(void);
+#endif
+
+void sleep_us(unsigned int);
+ssize_t readn(int ,void *, size_t);
+ssize_t writen(int, const void *, size_t);
+void daemonize(const char *);
+
+int s_pipe(int *);
+int recv_fd(int , ssize_t (*func)(int, const void *, size_t));
+int send_fd(int ,int);
+int send_err(int, int, const char *);
+int serv_listen(const char*);
+int serv_accept(int ,uid_t *);
+int cli_conn(const char *);
+int buf_args(char *, int (*func)(int, char**));
+
+int ptym_open(char *, int);
+int ptys_open(char *);
+
 #endif /* _APUE_H */
 
 
