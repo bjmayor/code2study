9d8134d50c64629f9001b9ff129a255031e0f66f
maynard <shunping.liu@huangjinqianbao.com>
2016-06-12 11:56:11 +0800

    强制性记录锁

--- advanced_programming_in_the_unix_environment/lock_reg.c
+++ advanced_programming_in_the_unix_environment/lock_reg.c
@@ -37,9 +37,9 @@ int lockfile(int fd)
 {
     struct flock fl;
 
-    lock.l_type = F_WRLCK;
-    lock.l_start = 0;
-    lock.l_whence = SEEK_SET;
-    lock.l_len = 0;
+    fl.l_type = F_WRLCK;
+    fl.l_start = 0;
+    fl.l_whence = SEEK_SET;
+    fl.l_len = 0;
     return (fcntl(fd, F_SETLK, &fl));
 }
